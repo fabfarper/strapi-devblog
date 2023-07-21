d1c0b740bd91b946c0a9a56bc73c13cd3c477e10
Fabio Faria Pereira <fabio.faria-pereira@alterway.fr>
2023-07-21 17:34:14 +0200

    fix sendgrid email config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,26 +6,9 @@ module.exports = ({ env }) => ({
         apiKey: env("SENDGRID_API_KEY"),
       },
       settings: {
-        defaultFrom: "",
-        defaultReplyTo: "",
+        defaultFrom: "fabio.fariapereira@smile.fr",
+        defaultReplyTo: "fabio.fariapereira@smile.fr",
       },
     },
-    // config: {
-    //   provider: "nodemailer",
-    //   providerOptions: {
-    //     host: env("SMTP_HOST"),
-    //     port: env("SMTP_PORT"),
-    //     auth: {
-    //       user: env("SMTP_USERNAME"),
-    //       pass: env("SMTP_PASSWORD"),
-    //     },
-    //     // ... any custom nodemailer options
-    //   },
-    //   secure: true,
-    //   settings: {
-    //     defaultFrom: "surfix@gmail.com",
-    //     defaultReplyTo: "surfix@gmail.com",
-    //   },
-    // },
   },
 });
--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,18 @@
-module.exports = {
+module.exports = ({ env }) => ({
   "github-projects": {
     enabled: true,
     resolve: "./src/plugins/github-projects",
   },
-};
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "fabio.fariapereira@smile.fr",
+        defaultReplyTo: "fabio.fariapereira@smile.fr",
+      },
+    },
+  },
+});
