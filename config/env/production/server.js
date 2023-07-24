0856be8932db5760e6693a0a58eb0c71a1981f3e
Fabio Faria Pereira <fabio.faria-pereira@alterway.fr>
2023-07-24 11:03:27 +0200

    add public server custom domain url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,3 @@
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL", "APP_URL"), // Sets the public URL of the application.
+  url: env("PUBLIC_SERVER_URL"), // Sets the public URL of the application.
 });
