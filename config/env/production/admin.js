7885dc35366bd9e69e46403aa9a77a69e534ea63
Fabio Faria Pereira <fabio.faria-pereira@alterway.fr>
2023-07-24 11:43:15 +0200

    add admin panel standalone serving config

--- config/env/production/admin.js
+++ config/env/production/admin.js
@@ -1,1 +1,6 @@
-module.exports = ({ env }) => ({});
+module.exports = ({ env }) => {
+  return {
+    url: env("PUBLIC_ADMIN_URL", "/admin"),
+    serveAdminPanel: env("PUBLIC_ADMIN_URL") == undefined, // If no public admin url, serve the Admin Panel on the same server
+  };
+};
